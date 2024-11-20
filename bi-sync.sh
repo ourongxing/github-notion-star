6704c117e3e1f812c7650f2a74b02331bd56c74b
Ou <orongxing@gmail.com>
2024-11-20 23:44:47 +0800

    update

--- bi-sync.sh
+++ bi-sync.sh
@@ -1,4 +1,4 @@
-cd ~/Development/github-notion-star/
+cd /Volumes/Extra/Development/github-notion-star/
 echo $(date +"%Y-%m-%d %H:%M") > bi-sync
 git add bi-sync
 git commit -m "bi-sync: $(date +"%Y-%m-%d %H:%M")"
